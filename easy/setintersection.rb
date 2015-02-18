afa1dc54c152c9b42c065d1d5f397ff02b90fe86
stran <stran@redhat.com>
2015-02-18 14:16:31 -0500

    Solved Set Intersection Challenge
    Only a jabronii doesn't think about newlines

--- easy/setintersection.rb
+++ easy/setintersection.rb
@@ -1,13 +1,13 @@
 =begin
 Steve Tran
 Set Intersection Solution
-Feb 17, 2015
+Took several tries to figure out that the \n at the end were throwing things off
+Feb 18, 2015
 =end
-
 File.open(ARGV[0]).each_line do |line|
-  a = line.split ";"
+  a = line.strip.split ";"
   a1 = a[0].split ","
   a2 = a[1].split ","
-  print (a1 & a2).join(",") unless (a1&a2).empty?
-  puts
+  puts (a1 & a2).join(",")
 end
+
