0d0df51aaf23efc64d7c32c881a39d7b72f34903
stran <stran@redhat.com>
2015-02-27 22:31:36 -0500

    Pass Triangle attempts.
    Still doesn't work, and losing interest in fixing it

---
+++ moderate/passtriangle2.rb
@@ -0,0 +1,33 @@
+=begin
+Steve Tran
+Pass Triangle Solution 
+Feb 27, 2015
+=end
+idx = 0
+sum = 0
+File.open(ARGV[0]).each_line do |line|
+  input = line.split(" ").map(&:to_i)
+  
+  center = input[idx]
+  right = input[idx+1] unless idx+1 > input.size
+
+  #puts "Left #{left}, Center #{center}, Right #{right} "
+
+  if input.size == 1
+    sum += center
+    idx = 0
+  #  puts "initial start in center"
+  else 
+ #   puts "moving!"
+    d = []
+    #d.push left unless !left
+    d.push center
+    d.push right unless !right
+  
+    #puts input.index(d.max)
+    sum += d.max
+    idx = input.index(d.max)
+  end
+end
+
+puts sum
