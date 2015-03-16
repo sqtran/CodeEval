3d222aec5f19ea2624e3481d4b03d62925a75631
stran <stran@redhat.com>
2015-03-16 01:51:29 -0400

    String List attempt number 2

---
+++ hard/stringlist.rb
@@ -0,0 +1,16 @@
+=begin
+Steve Tran
+String list solution
+Mar 16, 2015
+=end
+File.open(ARGV[0]).each_line do |line| 
+  input = line.split ","
+  num = input[0].to_i
+  letters = input[1].strip.split ""
+  cross = letters
+  for i in (1..num-1) do
+    cross = cross.product(letters).uniq
+  end
+  cross = cross.product().uniq unless num > 1
+  puts cross.collect { |e| e.flatten }.collect { |e| e.join "" }.sort.join ","
+end
