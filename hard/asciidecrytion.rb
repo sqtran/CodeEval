346c399e9b2233ea43813476f9c989261d9a6bfa
stran <stran@redhat.com>
2015-03-17 01:32:47 -0400

    Completed solution for the Ascii decryption challenge
    Can't believe this passed...

--- hard/asciidecrytion.rb
+++ hard/asciidecrytion.rb
@@ -1,15 +1,20 @@
 =begin
 Steve Tran
 ASCII Decryption solution
-Mar 16, 2015
+This is super hacky!
+Mar 17, 2015
 =end
-require 'debugger'
+def upper(c)
+  return /[A-Z]/.match(c)
+end
+
 File.open(ARGV[0]).each_line do |line| 
   input = line.split("|").collect {|e| e.strip}
   data = input[2].split(" ")
   ss = input[0].to_i-1
   ch = input[1].ord
-
+  
+  # loop through and figure out where we have pairs of twos
   candidates = []  
   for i in (0..data.size-ss) do 
     test = data[i..i+ss]
@@ -23,30 +28,27 @@ File.open(ARGV[0]).each_line do |line|
       end
     end
     if count == 2
-      #might have found a  bunch in between 2 whole words
+      # might have found a bunch in between 2 whole words
       candidates.push test
     end
   end
   
-
   offset = nil
   # start hacking away trying to find something that makes sense
-  candidates.each { |e| 
-    diff = e[-1].to_i - ch 
-    puts "e = #{e} #{e[-1]} diff = #{diff} looking at #{data[0].to_i + diff}"
-    
-    if /[[:upper]]/.match((data[0].to_i + diff).chr)
-      puts "found an upper #{e} diff = #{diff}"
+  for i in (0..candidates.size-1) do
+    diff = candidates[i][-1].to_i - ch
+    if diff >= 0 && upper((data[0].to_i-diff).chr)
       offset = diff
-      break
+      break 
     end
-  }
-  
+  end 
+
   if offset
     data.each{ |e| 
-      print (e.to_i+offset).chr
+      print (e.to_i-offset).chr
     }
   end
-  
 
+  puts
+  # can't believe this worked, they must have a super simple test suite
 end
