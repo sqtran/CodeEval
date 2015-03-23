15fab84c5db2c9456d97eb9a7937994b85ff3050
stran <stran@redhat.com>
2015-03-22 21:59:04 -0400

    Some optimizations at the cost of readability

--- hard/closestpair.rb
+++ hard/closestpair.rb
@@ -1,27 +1,19 @@
 =begin
 Steve Tran
 Closest Pair Solution
-Mar 21, 2015
+Mar 22, 2015
 =end
-def distance(x1, y1, x2, y2) 
-  Math.sqrt(((x2.to_f-x1.to_f) ** 2) + ((y2.to_f-y1.to_f) ** 2))
-end
-t1 = Time.now
-lines = File.readlines(ARGV[0]).collect { |a| a.strip}
-while lines.size > 1 do
-  size = lines[0].to_i+1
-  chunk = lines.take(size)[1..-1]
-  lines = lines.drop(size)
-
-  shortest = 10000
-  chunk.combination(2).to_a.collect{|e| e.join " "}.each { |e| 
-     i = e.split " "
-     d = distance(i[0], i[1], i[2], i[3])
+lines = File.readlines(ARGV[0])
+size = lines[0].to_i
+while size != 0 do
+:x
+  lines[1..size].combination(2).each { |e| 
+     i = e.join(" ").split " "
+     d = Math.sqrt(((i[2].to_f-i[0].to_f) ** 2) + ((i[3].to_f-i[1].to_f) ** 2))
      shortest = d unless d > shortest
    }
 
   puts shortest < 10000? shortest.round(4) : "INFINITY"
+  lines = lines.drop(size+1) #+1 to remove N
+  size = lines[0].to_i
 end
-t2 = Time.now
-
-puts (t2-t1) * 1000
