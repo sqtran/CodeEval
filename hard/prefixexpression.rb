10983264cfdbdf209a3d2178a2f18bbe4b2fa77c
Steve <stran@redhat.com>
2015-03-29 19:10:28 -0400

    Prefix Evaluation Solution
    What was so hard about this?

---
+++ hard/prefixexpression.rb
@@ -0,0 +1,23 @@
+=begin
+Steve Tran
+Prefix Expressions Solution
+Mar 29, 2015
+=end
+
+def is_num(n)
+  return n.to_s.match(/[0-9]+/)
+end
+
+File.open(ARGV[0]).each_line do |line|
+  stack = []
+  
+  line.strip.split(" ").reverse.each { |e|
+    if !is_num(e)
+      stack.push eval("#{stack.pop}#{e}#{stack.pop}")
+    else
+      stack.push e.to_f
+    end
+  }
+
+  puts stack[0].round
+end
