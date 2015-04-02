b45f4b1c207fc88cfbaa3318520100475032c94a
stran <stran@redhat.com>
2015-04-01 22:24:46 -0400

    Email Validation final solution

--- moderate/emailvalidation.rb
+++ moderate/emailvalidation.rb
@@ -1,10 +1,8 @@
 =begin
 Steve Tran
 Email validation solution
-Mar 19, 2015
+Apr 1, 2015
 =end
-
 File.open(ARGV[0]).each_line do |line|
-  #puts line.strip.match(/^[\w\d+.]+@([\w\d]+.)+[a-z]{3,3}$/) ? "true" : "false"
-  puts line.strip.match(/^[a-zA-Z0-9._+-]+@[a-z0-9.]+.[a-z]{3}$/) ? "true" : "false"  
+  puts line.strip.match(/^([a-zA-Z0-9._+-]+|".*")@[a-z0-9.]+.[a-z]{3}$/) ? "true" : "false"  
 end
