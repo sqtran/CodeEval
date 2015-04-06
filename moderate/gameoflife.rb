d35a48fdbb0d5764fc8985917ebfec3530613e49
stran <stran@redhat.com>
2015-04-06 13:55:14 -0400

    Replaced calculation with member variable for optimization

--- moderate/gameoflife.rb
+++ moderate/gameoflife.rb
@@ -7,8 +7,8 @@ $stale = false
 def step
   return unless !$stale
 
-  (0..$grid.size-1).each { |ro|
-    (0..$grid.size-1).each { |co|
+  (0..$n).each { |ro|
+    (0..$n).each { |co|
       n = neighbors(ro,co)
       if n < 2
         $a[ro][co] = '.'
@@ -24,9 +24,9 @@ def step
     return
   end
 
-  (0..$grid.size-1).each { |ro|
+  (0..$n).each { |ro|
     if $grid[ro] != $a[ro]
-      (0..$grid.size-1).each { |co|
+      (0..$n).each { |co|
         $grid[ro][co] = $a[ro][co]
       }
     end
@@ -35,15 +35,14 @@ end
 
 def neighbors(r, c)
   sum = 0
-  n = $grid.size-1
   sum += 1 unless !(r-1 >= 0 && $grid[r-1][c] == '*')
   sum += 1 unless !(c-1 >= 0 && $grid[r][c-1] == '*')
-  sum += 1 unless !(r+1 <= n && $grid[r+1][c] == '*')
-  sum += 1 unless !(c+1 <= n && $grid[r][c+1] == '*')
+  sum += 1 unless !(r+1 <= $n && $grid[r+1][c] == '*')
+  sum += 1 unless !(c+1 <= $n && $grid[r][c+1] == '*')
   sum += 1 unless !(c-1 >= 0 && r-1 >= 0 && $grid[r-1][c-1] == '*')
-  sum += 1 unless !(c-1 >= 0 && r+1 <= n && $grid[r+1][c-1] == '*')
-  sum += 1 unless !(c+1 <= n && r+1 <= n && $grid[r+1][c+1] == '*')
-  sum += 1 unless !(c+1 <= n && r-1 >= 0 && $grid[r-1][c+1] == '*')
+  sum += 1 unless !(c-1 >= 0 && r+1 <= $n && $grid[r+1][c-1] == '*')
+  sum += 1 unless !(c+1 <= $n && r+1 <= $n && $grid[r+1][c+1] == '*')
+  sum += 1 unless !(c+1 <= $n && r-1 >= 0 && $grid[r-1][c+1] == '*')
   sum
 end
 
@@ -52,9 +51,10 @@ File.open(ARGV[0]).each_line do |line|
   $grid.push line.strip.split ""
 end
 
+$n = $grid.size-1
 $a = Array.new ($grid.size) {Array.new}
-(0..$grid.size-1).each { |ro|
-  (0..$grid.size-1).each { |co|
+(0..$n).each { |ro|
+  (0..$n).each { |co|
     $a[ro][co] = $grid[ro][co]
   }
 }
