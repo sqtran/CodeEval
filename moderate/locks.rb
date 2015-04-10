aac23b354cc9c8a887384ba2c4ed8faffe6d3fff
stran <stran@redhat.com>
2015-04-10 18:48:25 -0400

    Fixed bug with Locks solution

--- moderate/locks.rb
+++ moderate/locks.rb
@@ -1,25 +1,17 @@
 =begin
 Steve Tran
 Locks solution
-Mar 19, 2015
+Apr 10, 2015
 =end
-
 File.open(ARGV[0]).each_line do |line|
-  input = line.split(" ").map(&:to_i)  
-  doors = [false] * input[0]
-  p doors
+  input = line.strip.split(" ").map!(&:to_i)
+
+  doors = Array.new(input[0]) {false}
   (1..input[1]-1).each {
-    doors = doors.each_with_index.map { |d, idx| (idx > 0 && idx+1 % 2 == 0) ? !d : d }
-    p doors
-    doors = doors.each_with_index.map { |d, idx| (idx > 0 && idx+1 % 3 == 0) ? !d : d }
-    p doors
+    doors = doors.map.with_index { |d, idx| (idx+1) % 2 == 0 ? true : d }
+    doors = doors.map.with_index { |d, idx| (idx+1) % 3 == 0 ? !d : d }
   }
+  # last iteration, he just changes the last door
   doors[-1] = !doors[-1]
-  p doors
-  
   puts doors.count{ |d| !d }
 end
-
-
-#I#0 1 2 3 4 5 6 7 8 9
-#D#1 2 3 4 5 6 7 8 9 10
