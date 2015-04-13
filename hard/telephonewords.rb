9b3770dd9fe69af693417b2e88d86fe01025286d
stran <stran@redhat.com>
2015-04-13 17:03:52 -0400

    slight optimization to Telephone Words

--- hard/telephonewords.rb
+++ hard/telephonewords.rb
@@ -3,31 +3,24 @@ Steve Tran
 Telephone words solution
 Apr 13, 2015
 =end
-
 $characters = Hash.new
-DATA.each_line do |line| 
-  input = line.strip.split " "
-  $characters[input[0]] = input[1].split ""
-end
+$characters["0"] = ["0"]
+$characters["1"] = ["1"]
+$characters["2"] = ["a","b","c"]
+$characters["3"] = ["d","e","f"]
+$characters["4"] = ["g","h","i"]
+$characters["5"] = ["j","k","l"]
+$characters["6"] = ["m","n","o"]
+$characters["7"] = ["p","q","r","s"]
+$characters["8"] = ["t","u","v"]
+$characters["9"] = ["w","x","y","z"]
 
 File.open(ARGV[0]).each_line do |line|
   input = line.strip.split ""
 
   arr = $characters[input.shift]
-  input.each {|e| 
+  input.each {|e|
     arr = arr.product($characters[e])
   }
   puts arr.collect! {|e| e.flatten.join ""}.join ","
 end
-
-__END__
-0 0
-1 1
-2 abc
-3 def
-4 ghi
-5 jkl
-6 mno
-7 pqrs
-8 tuv
-9 wxyz
