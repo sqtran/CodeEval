79960445a0d81abc2b847aa210813f1463a86194
Steve <stran@redhat.com>
2015-05-15 16:33:46 -0400

    Added Swap Numbers solution.
    Renamed a previous solution because it conflicted with new name.

--- easy/swapnumbers.rb
+++ easy/swapnumbers.rb
@@ -1,16 +1,9 @@
 =begin
 Steve Tran
-Swap Numbers solution
-Mar 20, 2015
+Swap Donkeys Solution
+May 15, 2015
 =end
-File.open(ARGV[0]).each_line do |line|
-  input = line.split ":" 
-  nums = input[0].split " "  
-  swps = input[1].split ","
 
-  swps.each { |e|
-    pos = e.split("-").map(&:to_i)
-    nums[pos[0]], nums[pos[1]] = nums[pos[1]], nums[pos[0]]
-  }
-  puts nums.join " "
+File.open(ARGV[0]).each_line do |line|
+  puts line.strip.split(" ").each { |e| e[0],e[-1] = e[-1],e[0] }.join(" ")
 end
